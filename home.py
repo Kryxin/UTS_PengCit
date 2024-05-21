f1ac0d3f39d71e4c2ad75e3bc9af9a28efddcbee
Kryxin <115473897+Kryxin@users.noreply.github.com>
2024-05-21 22:17:48 +0700

    Add files via upload

---
+++ home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title('Profile')
+st.subheader('Nama : Dinda Aurelia Zalsabella')
+st.subheader('NIM : 312210045')
+st.subheader('Kelas : TI.22.B1')
+st.subheader('Prodi : Teknik Infomatika')
+
+st.image("./img/Profile.jpg", caption="Bella", use_column_width="Always", width=200)
+
+
+st.link_button("Instagram", "https://www.instagram.com/dindaaureliaz?igsh=MXhtaDBjdnVkbHZ2cw%3D%3D&utm_source=qr")
+st.link_button("Github", "https://github.com/Kryxin")
